2e6e2552c647c87167429e6f0551f30318a871be
ahmedadelhashem <ahmedadela428@gmail.com>
2024-10-21 03:28:34 +0200

    incomeitem model

---
+++ lib/model/itemincome.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Itemincome{
+    final Color color;
+  final String title,trailing;
+
+  Itemincome({required this.color, required this.title, required this.trailing});
+}
