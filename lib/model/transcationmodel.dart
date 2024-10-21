86b61600d546d9362b127e144613cd95dc8d7d4e
ahmedadelhashem <ahmedadela428@gmail.com>
2024-10-21 03:29:01 +0200

    transcation model

---
+++ lib/model/transcationmodel.dart
@@ -0,0 +1,7 @@
+class Transcationmodel{
+  final String title,data,amout;
+  final bool iswithdrawal;
+
+
+  Transcationmodel({required this.title, required this.data, required this.amout, required this.iswithdrawal});
+  }
